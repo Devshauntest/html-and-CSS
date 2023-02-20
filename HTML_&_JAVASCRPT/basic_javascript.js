d7dc62bee3d0bc540aae74e1a00765c3237fe568
Tony <120454698+Devshauntest@users.noreply.github.com>
2023-02-20 03:19:22 +0100

    Add files via upload

---
+++ HTML_&_JAVASCRPT/basic_javascript.js
@@ -0,0 +1,9 @@
+
+alert("good morning");
+
+function my_First_Function() {
+    var string = "Kiss me, i am Irish";
+    var result = string.fontcolor("green");
+    document.getElementById("Irish").innerHTML = string;
+
+}
